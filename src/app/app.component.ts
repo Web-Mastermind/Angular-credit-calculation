badf95ee4edf2a1f65ccda29e0683f4078f786f8
Web-Mastermind <AZEAgent@gmail.com>
2024-07-04 00:18:46 +0400

    Credit Calcutalor Done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,17 +24,47 @@ import { FormsModule } from '@angular/forms';
     </div>
     <hr>
     <h1>{{result}}</h1>
+    <hr>
+    <table>
+      <thead>
+       <tr>
+        <th>Taksit |</th>
+        <th>Taksit Tutarı |</th>
+        <th>Qalan geri ödəmə</th>
+       </tr>
+      </thead>
+      <tbody>
+        @for(data of odemePlani; track data){
+          <tr>
+            <td>{{$index + 1}}</td>
+            <td>{{data.taksitTutari}}</td>
+            <td>{{data.qalangeriodeme}}</td>
+          </tr>
+        }
+      </tbody>
+    </table>
   `
 })
 export class AppComponent {
   kreditTutari: number = 0;
   taksitSayisi: number = 3;
   taksitler: number[] = [3, 6, 12, 24];
+  odemePlani: { taksitTutari: number, qalangeriodeme: number }[] = [];
   result: string = "";
 
   hesabla() {
     const taksitTutari = (this.kreditTutari / this.taksitSayisi) * 1.29
-    const toplamGeriOdeme = taksitTutari * this.taksitSayisi
+    let toplamGeriOdeme = taksitTutari * this.taksitSayisi
     this.result = `Taksit Tutarı: ${taksitTutari} - Taksit sayı: ${this.taksitSayisi} - Toplam geri ödəmə: ${toplamGeriOdeme}`
+    this.odemePlani = [];
+    for (let i = 0; i < this.taksitSayisi; i++) {
+      toplamGeriOdeme -= taksitTutari
+      const data = {
+        taksitTutari: taksitTutari,
+        qalangeriodeme: toplamGeriOdeme
+      }
+      this.odemePlani.push(data)
+
+    }
   }
 }
