8d6deceb89351ef144453679ccd48aafca3e6457
Web-mastermind <azeagent@gmail.com>
2024-07-03 23:26:42 +0400

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   template: `
-    <h1>Credit Calculation</h1>asdadsasd
+    <h2>Credit Calculation</h2>
+    <div>
+      <label>Kredit Tutarı</label>
+      <input [(ngModel)]="kreditTutari">
+    </div>
+    <div>
+      <label>Taksit sayısı</label>
+      <select [(ngModel)]="taksitSayisi">
+        @for(data of taksitler; track data){
+          <option>{{data}}</option>
+        }
+      </select>
+    </div>
+    <div>
+      <button (click)="hesabla()">Hesabla</button>
+    </div>
+    <hr>
+    <h1>{{result}}</h1>
   `
 })
 export class AppComponent {
-  title = 'credit-calculation';
+  kreditTutari: number = 0;
+  taksitSayisi: number = 3;
+  taksitler: number[] = [3, 6, 12, 24];
+  result: string = "";
+
+  hesabla() {
+    const taksitTutari = (this.kreditTutari / this.taksitSayisi) * 1.29
+    const toplamGeriOdeme = taksitTutari * this.taksitSayisi
+    this.result = `Taksit Tutarı: ${taksitTutari} - Taksit sayı: ${this.taksitSayisi} - Toplam geri ödəmə: ${toplamGeriOdeme}`
+  }
 }
